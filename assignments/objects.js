ad5e4404f0f2593d5ac5875737ec2b741c44e8dd
fromthehart <fromthehart@pm.me>
2019-05-05 14:53:19 -0400

    Added multiplyNums(a,b) method to object antonietta

--- assignments/objects.js
+++ assignments/objects.js
@@ -42,11 +42,14 @@ const gannie = {
   "gender": "M"
 }
 
-const antoinetta = {
+const antonietta = {
   "id": 5,
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
-  "gender": "F"
+  "gender": "F",
+  "multiplyNums": function(a, b) {
+    return a * b;
+  }
 }
 
 // ==== Challenge 2: Reading Object Data ==== 
@@ -65,7 +68,7 @@ console.log(keven.email);
 console.log(gannie.name);
 
 // Antonietta's Gender
-console.log(antoinetta.gender);
+console.log(antonietta.gender);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
@@ -75,6 +78,8 @@ console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
